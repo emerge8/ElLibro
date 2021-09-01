8c4974a6f1281a48903974f5f97fc767298e9a61
Emerson Luiz <emerson_egl@hotmail.com>
2021-09-01 00:03:30 -0300

    Project created

---
+++ backend/src/main/java/com/thebooks/elLibros/ElLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.thebooks.elLibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElLibrosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thebooks/elLibros/ElLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebooks.elLibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
